c6bd3c629b40ada2161396cbececb50e7b2774cf
seastian <sebastian@momentos.dk>
2016-01-08 10:15:51 +0100

    modified h5bp source file to avoid no-transform

--- lapela/app.js
+++ lapela/app.js
@@ -12,8 +12,7 @@ var h5bp = require('h5bp');
 var app = express();
 var logStream = fs.createWriteStream('logs/access.log',{flags: 'a'});
 
-//app.use(h5bp({ root: __dirname + '../public' }));
- 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,13 +24,18 @@ if (app.get('env') == 'production') {
 } else {
   app.use(logger('dev'));
 }
+
+app.use(h5bp({ root: __dirname + '../public' }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- lapela/routes/index.js
+++ lapela/routes/index.js
@@ -6,7 +6,7 @@ var publicDir = path.join(__dirname, '../public');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.set('Cache-Control', 'public,max-age=0');
-  res.sendFile('../views/index.html');
+  res.sendFile(publicDir, 'index.html');
 });
 
 router.get('/[^\.]+$', function(req, res){
